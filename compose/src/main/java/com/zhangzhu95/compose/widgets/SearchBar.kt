1eaff99c5f9f12c12906b6d0aa45df92a6a4b3f9
Zakaria Boukaddouss <zak.bouka@gmail.com>
2022-12-20 20:55:59 +0100

    fix unused imports

--- compose/src/main/java/com/zhangzhu95/compose/widgets/SearchBar.kt
+++ compose/src/main/java/com/zhangzhu95/compose/widgets/SearchBar.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
--- feat/search/src/main/java/com/zhangzhu95/search/SearchNavScreen.kt
+++ feat/search/src/main/java/com/zhangzhu95/search/SearchNavScreen.kt
@@ -1,7 +1,5 @@
 package com.zhangzhu95.search
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
@@ -16,7 +14,6 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
